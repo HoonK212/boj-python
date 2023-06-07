0dcb391ba839438b2660553e4214f5e14d28e7b0
HoonK212 <b0aeiou@gmail.com>
2023-06-07 20:42:59 +0900

    1783

---
+++ 1783.py
@@ -0,0 +1,12 @@
+import sys; input = sys.stdin.readline; sys.setrecursionlimit(10**6)
+if __name__ == '__main__':
+
+  n, m = map(int, input().split())
+  if n == 1:
+    print(1)
+  elif n == 2:
+    print(min(4, (m + 1) // 2))
+  elif m <= 6:
+    print(min(4, m))
+  else:
+    print(m - 2)
