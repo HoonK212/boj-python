ce8b20a0fefa14b14466e9000359cbb63a1b594d
HoonK212 <b0aeiou@gmail.com>
2023-06-15 16:44:31 +0900

    Update 10815

--- 10815.py
+++ 10815.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
   m = int(input())
   second_card_list = list(map(int, input().split()))
 
+  # 같은 숫자가 존재하는 경우에만 bisect_left와 bisect_right의 index 값이 달라진다는 것이 핵심 !!!
   for card in second_card_list:
     l = bisect_left(first_card_list, card)
     r = bisect_right(first_card_list, card)
