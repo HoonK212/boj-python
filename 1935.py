477f6022b1b21f9989343195b76ce9112284be0b
HoonK212 <b0aeiou@gmail.com>
2023-08-18 16:55:40 +0900

    Update 1935

--- 1935.py
+++ 1935.py
@@ -3,23 +3,22 @@ if __name__ == '__main__':
 
   n = int(input())
   expression = list(str(input()).rstrip())
-  nums = [int(input()) for _ in range(n)]
+  nums = [float(input()) for _ in range(n)]
 
   i = 2
   while len(expression) >= 3:
-    if expression[i].isnumeric() or expression[i].isalpha():
+    if expression[i].isalpha():
       i = i + 1
     else:
       if expression[i] == '+':
-        expression[i] = nums[i - 2] + nums[i -1]
+        nums.append(nums[ord(expression.pop(i - 2)) - 65] + nums[ord(expression.pop(i - 2)) - 65])
       elif expression[i] == '-':
-        expression[i] = nums[i - 2] - nums[i -1]
+        nums.append(nums[ord(expression.pop(i - 2)) - 65] - nums[ord(expression.pop(i - 2)) - 65])
       elif expression[i] == '*':
-        expression[i] = nums[i - 2] * nums[i -1]
+        nums.append(nums[ord(expression.pop(i - 2)) - 65] * nums[ord(expression.pop(i - 2)) - 65])
       elif expression[i] == '/':
-        expression[i] = nums[i - 2] / nums[i -1]
-      expression.pop(i - 2)
-      expression.pop(i - 2)
-      i = i -2
+        nums.append(nums[ord(expression.pop(i - 2)) - 65] / nums[ord(expression.pop(i - 2)) - 65])
+      expression[i - 2] = chr(len(nums) - 1 + 65)
+      i = i - 2
 
-  print(expression[0])
+  print("{:.2f}".format(nums[-1]))
