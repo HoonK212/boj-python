e3b214fc575579a5a7013b19af0096ef03307c29
HoonK212 <b0aeiou@gmail.com>
2023-08-21 16:25:17 +0900

    13904

---
+++ 13904.py
@@ -0,0 +1,22 @@
+import sys; input = sys.stdin.readline
+import heapq
+if __name__ == '__main__':
+
+  n = int(input())
+  assignments = [[] for _ in range(1001)]
+  max_d = 0
+
+  for _ in range(n):
+    d, w = map(int, input().split())
+    assignments[d].append(w)
+    max_d = max(max_d, d)
+
+  heap = []
+  answer = 0
+  for d in range(max_d, 0, -1):
+    for w in assignments[d]:
+      heapq.heappush(heap, -w)
+    if heap:
+      answer = answer + -heapq.heappop(heap)
+
+  print(answer)
