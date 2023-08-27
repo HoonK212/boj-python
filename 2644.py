2b33775aabf99e1c7a615ed2c9594bccb5597765
HoonK212 <b0aeiou@gmail.com>
2023-08-27 13:14:18 +0900

    Update 2644

--- 2644.py
+++ 2644.py
@@ -2,46 +2,41 @@ import sys; input = sys.stdin.readline
 from collections import deque
 if __name__ == '__main__':
 
-  def dfs(start, end):
+  def bfs(start):
 
-    queue = deque()
-    queue.append(node_links[start])
-    answer = 1
-    visited_nodes = [False for _ in range(n + 1)]
-    visited_nodes[start] = True
+    queue = deque([start])
+    visited[start] = True
 
     while queue:
-      node_link = queue.pop()
+      node = queue.pop()
 
-      if end in node_link:
-        return answer
-
-      for node in node_link:
-        if not visited_nodes[node]:
-          queue.append(node_links[node])
-          visited_nodes[node] = True
-        answer = answer + 1
-        break
-
-      if not queue:
-        return -1
+      for link_node in node_links[node]:
+        if not visited[link_node]:
+          visited[link_node] = True
+          queue.append(link_node)
+          answer[link_node] = answer[node] + 1
 
   n = int(input())
-  node_links = [[] for _ in range(n + 1)]
-
   start_node, end_node = map(int, input().split())
-
   m = int(input())
+
+  node_links = [[] for _ in range(n + 1)]
   for _ in range(m):
     node1, node2 = map(int, input().split())
     node_links[node1].append(node2)
     node_links[node2].append(node1)
 
-  print(dfs(start_node, end_node))
+  visited = [False] * (n + 1)
+  answer = [0] * (n + 1)
 
-# 10
-# 7 10
-# 8
+  # start node를 기준으로 다른 모든 node와의 촌수를 계산 후, end node에 해당하는 값을 출력
+  bfs(start_node)
+  print(-1 if answer[end_node] == 0 else answer[end_node])
+
+# 사촌
+# 11
+# 10 11
+# 9
 # 1 2
 # 1 3
 # 2 7
@@ -49,4 +44,19 @@ if __name__ == '__main__':
 # 2 9
 # 4 5
 # 4 6
-# 9 10
+# 7 10
+# 9 11
+
+# 무촌
+# 11
+# 6 10
+# 9
+# 1 2
+# 1 3
+# 2 7
+# 2 8
+# 2 9
+# 4 5
+# 4 6
+# 7 10
+# 9 11
