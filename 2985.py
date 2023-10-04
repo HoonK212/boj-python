477ab5b7ab6d56569d3818b0f9a33dbc6c93d160
HoonK212 <b0aeiou@gmail.com>
2023-10-04 14:34:20 +0900

    Update 2985

--- 2985.py
+++ 2985.py
@@ -1,7 +1,9 @@
-import sys; input = sys.stdin.readline; sys.setrecursionlimit(10**6)
+import sys; input = sys.stdin.readline
 if __name__ == '__main__':
 
   a, b, c = map(int, input().split())
+
+  # 사칙연산 기호, 등호 순서인 경우
   if a + b == c:
     print(f"{a}+{b}={c}")
   elif a - b == c:
@@ -10,6 +12,8 @@ if __name__ == '__main__':
     print(f"{a}*{b}={c}")
   elif a // b == c:
     print(f"{a}/{b}={c}")
+
+  # 등호, 사칙연산 기호 순서인 경우
   elif a == b + c:
     print(f"{a}={b}+{c}")
   elif a == b - c:
