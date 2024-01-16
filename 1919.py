d288709fd83db9fef30548c6cdcbe07438326830
HoonK212 <b0aeiou@gmail.com>
2024-01-16 18:25:25 +0900

    Update 1919, 2170

--- 1919.py
+++ 1919.py
@@ -1,17 +1,27 @@
 import sys; input = sys.stdin.readline; sys.setrecursionlimit(10**6)
 if __name__ == '__main__':
 
+  # string a와 b 입력
   str_a = str(input().rstrip())
   str_b = str(input().rstrip())
 
+  # 각 알파벳 철자에 해당하는 arr 배열 생성
   arr = [0] * 26
+
+  # string a의 각 철자에 알맞는 i번째 배열 값 +=1
   for a in str_a:
-    arr[ord(a) - 97] += 1
+    i = ord(a) - 97
+    arr[i] += 1
+
+  # string b의 각 철자에 알맞는 i번째 배열 값 -=1
   for b in str_b:
-    arr[ord(b) - 97] -= 1
+    i = ord(b) - 97
+    arr[i] -= 1
 
   answer = 0
+
+  # 여전히 arr 배열에서 0이 아닌 값들은 애너그램 관계를 불가능하게 만드는 비대칭 철자의 수
   for i in arr:
-    answer += abs(i)
+    answer += abs(i) # 해당 값들을 절대값으로 합산
 
   print(answer)
--- 2170.py
+++ 2170.py
@@ -5,15 +5,26 @@ if __name__ == '__main__':
   lines = [list(map(int, input().split())) for _ in range(n)]
   lines.sort()
 
+  # 정렬된 line들 중 첫번째 line의 x, y를 start, end로 초기화
   start = lines[0][0]
   end = lines[0][1]
+
   answer = 0
 
+  # 정렬된 line들 중 두번째 line부터 마지막 line까지 반복
+  # x, y를 각각 cur_start, cur_end로 초기화
   for cur_start, cur_end in lines[1:]:
+
+    # cur_start가 기존의 end보다 크면, start~end까지의 길이를 합산하고,
+    # start를 cur_start로 초기화
     if cur_start > end:
       answer += end - start
       start = cur_start
+
+    # 기존의 end와 cur_end 중, 큰 값으로 end 초기화
     end = max(end, cur_end)
+
+  # 마지막으로 start~end까지의 길이 합산
   answer += end - start
 
   print(answer)
