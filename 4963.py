56cfb8626ce7b93d708580c0548e239883f402e7
HoonK212 <b0aeiou@gmail.com>
2024-03-24 14:27:27 +0900

    Update 4963

--- 4963.py
+++ 4963.py
@@ -2,17 +2,19 @@ import sys; input = sys.stdin.readline; sys.setrecursionlimit(10**6)
 if __name__ == '__main__':
 
   def dfs(y, x):
-    if x < 0 or x >= w or y < 0 or y >= h:
+    if x < 0 or x >= w or y < 0 or y >= h: # 범위를 멋어날 경우, 재귀 종료
       return False
 
-    if coordinates[y][x] == 1:
+    if coordinates[y][x] == 1: # 현재 좌표가 땅(1)인 경우, 해당 땅을 방문 처리 (0으로 변경)
       coordinates[y][x] = 0
 
+      # 상하좌우로 인접한 땅 탐색
       dfs(y - 1, x)
       dfs(y + 1, x)
       dfs(y, x - 1)
       dfs(y, x + 1)
 
+      # 대각선으로 인접한 땅 탐색
       dfs(y - 1, x - 1)
       dfs(y - 1, x + 1)
       dfs(y + 1, x - 1)
@@ -31,9 +33,10 @@ if __name__ == '__main__':
     for _ in range(h):
       coordinates.append(list(map(int, input().split())))
 
-    answer = 0
+    answer = 0 # 섬의 개수
     for i in range(h):
       for j in range(w):
-        if dfs(j, i):
+        if dfs(i, j):
           answer += 1
+
     print(answer)
